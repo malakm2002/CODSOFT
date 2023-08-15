74225ecc47ebadce4625882d639be90685ab4368
malakm2002 <93513976+malakm2002@users.noreply.github.com>
2023-08-15 15:35:56 +0300

    QUIZ - finalizing the app by debugging the error showing at the begining of the quiz

--- QuizApp/lib/frontend/Quizzes/Quiz.dart
+++ QuizApp/lib/frontend/Quizzes/Quiz.dart
@@ -60,6 +60,16 @@ class _QuizState extends State<Quiz> {
   void initState() {
     super.initState();
     currentUser = FirebaseAuth.instance.currentUser;
+    fetchQuestions(); // Fetch questions when the widget is initialized
+  }
+
+  void fetchQuestions() async {
+    final category = ModalRoute.of(context)!.settings.arguments as String;
+    Map<String, Map<String, dynamic>> fetchedQuestions =
+        await AccessDB.getRandomQuestions(category, 5);
+    setState(() {
+      questions = fetchedQuestions;
+    });
   }
 
   @override
@@ -83,149 +93,156 @@ class _QuizState extends State<Quiz> {
       body: Container(
         color: MyColors.myBlack,
         alignment: Alignment.center,
-        child: showScore
-            ? Center(
-                child: Container(
-                  margin: const EdgeInsets.all(20.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Each Question is worth 100 point',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 24, color: MyColors.myPrimaryPink),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Text(
-                        'Your Score is\n $score point',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 28, color: MyColors.myPrimaryPink),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Column(
-                        children: answersStatus.entries.map((entry) {
-                          return Container(
-                            alignment: Alignment.topCenter,
-                            margin: const EdgeInsets.all(5.0),
-                            child: Text(
-                              '${answers[entry.key]}: ${entry.value ? '✅' : '❌'}',
-                              style: TextStyle(
-                                  color: MyColors.myPrimaryPink, fontSize: 20),
-                            ),
-                          );
-                        }).toList(),
-                      ),
-                      QAElevatedButton(
-                          onPressed: () => {
-                                print(score),
-                                quiz = {
-                                  'email': currentUser?.email,
-                                  'questionsStatus': answersStatus,
-                                  'score': score,
-                                },
-                                print(score),
-                                AccessDB.addUserQuiz(quiz),
-                                Navigator.pushNamed(context, '/home')
-                              },
-                          buttonText: "Get Quizzed Again",
-                          color: MyColors.myPrimaryPink,
-                          textColor: MyColors.myBlack)
-                    ],
-                  ),
-                ),
+        child: questions.isEmpty
+            ? CircularProgressIndicator(
+                color: MyColors.myPrimaryPink,
               )
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Question ${currentQuestionIndex + 1}/${questions.length}',
-                    style: TextStyle(
-                        fontSize: 26,
-                        color: MyColors.myPrimaryPink,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(height: 30),
-                  Container(
-                    padding: const EdgeInsets.only(
-                        left: 50.0, top: 30.0, right: 50.0),
-                    child: Text(
-                      questions.values
-                          .elementAt(currentQuestionIndex)['question'],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: MyColors.myPrimaryPink,
-                        fontWeight: FontWeight.bold,
+            : showScore
+                ? Center(
+                    child: Container(
+                      margin: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'Each Question is worth 100 point',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 24, color: MyColors.myPrimaryPink),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            'Your Score is\n $score point',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 28, color: MyColors.myPrimaryPink),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Column(
+                            children: answersStatus.entries.map((entry) {
+                              return Container(
+                                alignment: Alignment.topCenter,
+                                margin: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  '${answers[entry.key]}: ${entry.value ? '✅' : '❌'}',
+                                  style: TextStyle(
+                                      color: MyColors.myPrimaryPink,
+                                      fontSize: 20),
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                          QAElevatedButton(
+                              onPressed: () => {
+                                    print(score),
+                                    quiz = {
+                                      'email': currentUser?.email,
+                                      'questionsStatus': answersStatus,
+                                      'score': score,
+                                    },
+                                    print(score),
+                                    AccessDB.addUserQuiz(quiz),
+                                    Navigator.pushNamed(context, '/home')
+                                  },
+                              buttonText: "Get Quizzed Again",
+                              color: MyColors.myPrimaryPink,
+                              textColor: MyColors.myBlack)
+                        ],
                       ),
                     ),
-                  ),
-                  SizedBox(height: 40),
-                  Column(
-                    children: (questions.values
-                            .elementAt(currentQuestionIndex)['options'] as Map)
-                        .entries
-                        .map((option) => ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  selectedAnswer = option.key;
-                                });
-                                if (selectedAnswer.isNotEmpty) {
-                                  checkAnswer(option.key);
-                                }
-                              },
-                              style: ButtonStyle(
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                    selectedAnswer == option.key
-                                        ? (answeredCorrectly
-                                            ? Colors.green
-                                            : Colors.red)
-                                        : MyColors.myPrimaryPink,
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Question ${currentQuestionIndex + 1}/${questions.length}',
+                        style: TextStyle(
+                            fontSize: 26,
+                            color: MyColors.myPrimaryPink,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 30),
+                      Container(
+                        padding: const EdgeInsets.only(
+                            left: 50.0, top: 30.0, right: 50.0),
+                        child: Text(
+                          questions.values
+                              .elementAt(currentQuestionIndex)['question'],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: MyColors.myPrimaryPink,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Column(
+                        children: (questions.values
+                                    .elementAt(currentQuestionIndex)['options']
+                                as Map)
+                            .entries
+                            .map((option) => ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      selectedAnswer = option.key;
+                                    });
+                                    if (selectedAnswer.isNotEmpty) {
+                                      checkAnswer(option.key);
+                                    }
+                                  },
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                        selectedAnswer == option.key
+                                            ? (answeredCorrectly
+                                                ? Colors.green
+                                                : Colors.red)
+                                            : MyColors.myPrimaryPink,
+                                      ),
+                                      padding: MaterialStateProperty.all(
+                                          const EdgeInsets.only(
+                                              top: 5,
+                                              bottom: 5,
+                                              left: 45,
+                                              right: 45)),
+                                      shape: MaterialStateProperty.all(
+                                        RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(40.0),
+                                        ),
+                                      )),
+                                  child: Text(
+                                    option.value,
+                                    style: TextStyle(fontSize: 18),
                                   ),
-                                  padding: MaterialStateProperty.all(
-                                      const EdgeInsets.only(
-                                          top: 5,
-                                          bottom: 5,
-                                          left: 45,
-                                          right: 45)),
-                                  shape: MaterialStateProperty.all(
-                                    RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(40.0),
-                                    ),
-                                  )),
-                              child: Text(
-                                option.value,
-                                style: TextStyle(fontSize: 18),
-                              ),
-                            ))
-                        .toList(),
+                                ))
+                            .toList(),
+                      ),
+                      SizedBox(
+                        height: 70.0,
+                      ),
+                      if (selectedAnswer.isNotEmpty && !submit)
+                        QAElevatedButton(
+                            onPressed: moveToNextQuestion,
+                            buttonText: 'Next Question',
+                            color: MyColors.myPrimaryPink,
+                            textColor: MyColors.myBlack),
+                      if (submit && selectedAnswer.isNotEmpty)
+                        QAElevatedButton(
+                            onPressed: () => {
+                                  setState(() {
+                                    showScore = true;
+                                  })
+                                },
+                            buttonText: 'Submit',
+                            color: MyColors.myPrimaryPink,
+                            textColor: MyColors.myBlack)
+                    ],
                   ),
-                  SizedBox(
-                    height: 70.0,
-                  ),
-                  if (selectedAnswer.isNotEmpty && !submit)
-                    QAElevatedButton(
-                        onPressed: moveToNextQuestion,
-                        buttonText: 'Next Question',
-                        color: MyColors.myPrimaryPink,
-                        textColor: MyColors.myBlack),
-                  if (submit && selectedAnswer.isNotEmpty)
-                    QAElevatedButton(
-                        onPressed: () => {
-                              setState(() {
-                                showScore = true;
-                              })
-                            },
-                        buttonText: 'Submit',
-                        color: MyColors.myPrimaryPink,
-                        textColor: MyColors.myBlack)
-                ],
-              ),
       ),
     );
   }
